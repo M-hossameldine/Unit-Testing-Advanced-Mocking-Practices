3a78c6a8f3c7ca87753b4fbc805e0c1dcd552ec4
Mohamed Hossam <m.hossameldine@gmail.com>
2022-06-06 21:14:37 +0200

    mocking frontend library - test axios requests

--- util/http.test.js
+++ util/http.test.js
@@ -1,8 +1,12 @@
-// Mocking global values & functions
 import { describe, it, expect, vi } from 'vitest';
 
-import { sendDataRequest } from './http';
+import { sendDataRequest, sendDataRequestWithAxios } from './http';
+import { axiosTestResponseData } from '../__mocks__/axios';
 
+// Mocking frontend library axios
+vi.mock('axios');
+
+// Mocking global values & functions
 const testResponseData = { dataKey: 'data value' };
 
 const testFetch = (url, options) => {
@@ -24,9 +28,19 @@ const testFetch = (url, options) => {
 vi.stubGlobal('fetch', testFetch);
 
 describe('sendDataRequest()', () => {
-  it('should always return responseData', () => {
+  it('should return any available response Data', () => {
     const testData = { key: 'test' };
 
     return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+  });
+});
+
+describe('sendDataRequestWithAxios()', () => {
+  it('should return any available response Data', () => {
+    const testData = { key: 'test' };
+
+    return expect(sendDataRequestWithAxios(testData)).resolves.toEqual(
+      axiosTestResponseData
+    );
   });
 });
