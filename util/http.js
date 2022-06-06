6106b42c06c2bda09255d6ba5df8af04113c7f6c
Mohamed Hossam <m.hossameldine@gmail.com>
2022-06-06 21:11:25 +0200

    add axios api

--- util/http.js
+++ util/http.js
@@ -1,5 +1,7 @@
-import {HttpError} from './errors.js';
+import { HttpError } from './errors.js';
+import axios from 'axios';
 
+// to apply on mocking global values
 export async function sendDataRequest(data) {
   const response = await fetch('https://dummy-site.dev/posts', {
     method: 'POST',
@@ -12,8 +14,36 @@ export async function sendDataRequest(data) {
   const responseData = await response.json();
 
   if (!response.ok) {
-    throw new HttpError(response.status, 'Sending the request failed.', responseData);
+    throw new HttpError(
+      response.status,
+      'Sending the request failed.',
+      responseData
+    );
   }
+
+  return responseData;
+}
+
+// to apply on mocking frontend libraries
+export async function sendDataRequestWithAxios(data) {
+  const responseData = axios({
+    method: 'post',
+    url: 'https://dummy-site.dev/posts',
+    data,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((data) => data)
+    .catch((error) => {
+      if (error.response) {
+        throw new HttpError(
+          error.response.status,
+          'Sending the request failed.',
+          error.response.data
+        );
+      }
+    });
 
   return responseData;
 }
